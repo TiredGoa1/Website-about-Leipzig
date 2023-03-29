63ab85f39b04c5c6a6caadc1fe0f6eaa87078a01
TiredGoa1 <chanyhelps@gmail.com>
2023-03-29 23:43:16 +0200

    memory revised

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -10,12 +10,44 @@ const cards=document.querySelectorAll(".Card");
 
 const orders=shuffle([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]);
 
+let aufgedeckteKarte = undefined; 
+
 cards.forEach((card,index) => {
     card.style.order = orders[index];
     card.style.backgroundColor="#000000";
 
     card.onclick = () =>{
-        card.style.backgroundColor=""
+        handleCardClick(card)
     }
 })
+
+
+
+function handleCardClick(card){
+    card.style.backgroundColor=""
+    if (aufgedeckteKarte){
+        const cardColor = card.dataset.color;
+        const aufgedeckteKarteColor = aufgedeckteKarte.dataset.color;
+        
+        if (cardColor == aufgedeckteKarteColor){
+            console.log("paar")
+        }
+        else{
+            const aufgedeckteKarteRef=aufgedeckteKarte;
+            
+            setTimeout( () => {
+                
+                
+                card.style.backgroundColor="#000000";
+                aufgedeckteKarteRef.style.backgroundColor="#000000";
+                
+            },2000)
+        }
+
+        aufgedeckteKarte = undefined;
+    }
+    else{
+        aufgedeckteKarte = card
+    }
+}
 
