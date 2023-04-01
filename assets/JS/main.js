4545606ede238765d2ea856b890d72ad3619d420
RAKIMON <mail@enesimran.net>
2023-04-01 04:30:40 +0200

    Added "disabled" Attributes to forbid doubleselection of card.

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -6,56 +6,62 @@ function shuffle(a) {
     return a;
 }
 
-const cards=document.querySelectorAll(".Card");
+const cards = document.querySelectorAll(".Card");
 
-const orders=shuffle([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]);
+const orders = shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]);
 
-let aufgedeckteKarte = undefined; 
+let aufgedeckteKarte = undefined;
 let matchedCards = [];
 let isProcessing = false;
 
-cards.forEach((card,index) => {
+cards.forEach((card, index) => {
     card.style.order = orders[index];
-    card.style.backgroundColor="#000000";
+    card.style.backgroundColor = "#000000";
+    card.setAttribute("disabled", false)
 
-    card.onclick = () =>{
-        if (!isProcessing && !matchedCards.includes(card)) {
-        handleCardClick(card);
+    card.onclick = () => {
+        if (!isProcessing & !matchedCards.includes(card) & card.getAttribute("disabled") == "false") {
+            console.log(card.dataset.color);
+            handleCardClick(card);
         }
     };
 });
 
 
 
-function handleCardClick(card){
-     if (aufgedeckteKarte) {
+function handleCardClick(card) {
+    if (aufgedeckteKarte) {
         // do not allow multiple cards to be selected at the same time
         isProcessing = true;
-     }
-
-    card.style.backgroundColor=""
-    if (aufgedeckteKarte){
+    }
+    card.style.backgroundColor = ""
+    card.setAttribute("disabled", true)
+    if (aufgedeckteKarte) {
         const cardColor = card.dataset.color;
         const aufgedeckteKarteColor = aufgedeckteKarte.dataset.color;
-        
-        if (cardColor == aufgedeckteKarteColor){
+
+        if (cardColor == aufgedeckteKarteColor) {
             console.log("paar")
             matchedCards.push(card, aufgedeckteKarte);
+            aufgedeckteKarte = undefined;
             isProcessing = false;
+            return;
         }
-        else{
-            const aufgedeckteKarteRef=aufgedeckteKarte;
+        else {
+            const aufgedeckteKarteRef = aufgedeckteKarte;
             isProcessing = true;
 
-            setTimeout( () => {
-                card.style.backgroundColor="#000000";
-                aufgedeckteKarteRef.style.backgroundColor="#000000";
+            setTimeout(() => {
+                card.style.backgroundColor = "#000000";
+                card.setAttribute("disabled", false)
+                aufgedeckteKarteRef.style.backgroundColor = "#000000";
+                aufgedeckteKarteRef.setAttribute("disabled", false)
                 isProcessing = false;
             }, 500);
         }
 
         aufgedeckteKarte = undefined;
-    }else{
+    } else {
         aufgedeckteKarte = card
     }
 };
